f06a98c9b15a474d072dc5d7337c21506005a7d5
balkiyaeva <amil.balkiyaeva@gmail.com>
2023-05-26 18:01:35 +0600

    removed unused classes

--- src/main/java/com/example/volunteer/repositories/MistakeRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.volunteer.repositories;
-
-
-import com.example.volunteer.entities.Mistakes;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MistakeRepo extends JpaRepository<Mistakes,Long> {
-}
